5e87c4e090c1e39690e0b493c35f37cfea2cb936
Jonathan Woster <jonathanwoster@gmail.com>
2023-04-22 13:00:17 -0500

    changes to fix js

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -1,6 +1,6 @@
 const d = new Date();
 const yearvalue = d.getFullYear();
-document.querySelector("#copyright-year").textContent = yearvalue;
+document.querySelector(".copyright-year").textContent = yearvalue;
 
 let lastmod = document.lastModified;
 document.getElementById("last-updated").innerHTML = lastmod;
