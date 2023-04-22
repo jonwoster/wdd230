6e7d4b22e38cb33cd057f111cb43d09400083282
Jonathan Woster <jonathanwoster@gmail.com>
2023-04-22 12:18:54 -0500

    changed copyright syntax

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -1,7 +1,7 @@
 
 const d = new Date();
 const yearvalue = d.getFullYear();
-document.querySelector("#copyright-year").textContent = yearvalue;
+document.querySelector("#copyright-year").innerHTML = yearvalue;
 
 let lastmod = document.lastModified;
 document.getElementById("last-updated").innerHTML = lastmod;
