76eadb77093280a2113744416451a5f823e25ef0
Jonathan Woster <jonathanwoster@gmail.com>
2023-05-18 18:51:01 -0500

    fixed for reminder banner

--- chamber/scripts/chamberscript.js
+++ chamber/scripts/chamberscript.js
@@ -9,6 +9,8 @@ document.getElementById("last-updated").innerHTML = lastmod;
 
 const datefieldUK = document.querySelector("#header-date");
 const now = new Date();
+const dayofweek = now.getDay(); // for determining if we want to display reminder
+// alert(dayofweek); for debugging
 const fulldateUK = new Intl.DateTimeFormat("en-UK", {dateStyle: "full"}).format(now);
 datefieldUK.innerHTML = `${fulldateUK}`;
 
@@ -22,3 +24,28 @@ function togglemenu() {
 
 const x = document.getElementById('hamburger-button');
 x.onclick = togglemenu;
+
+//Use JavaScript to display a banner on Mondays or Tuesdays only at the very top of the page that says 
+//🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m." Make sure your design matches your schema for the site.
+if(dayofweek == 1 || dayofweek == 4) {
+    // create variable that holds reference to header
+    const header = document.querySelector('header');
+    // create div element to hold the reminder, use mtgreminder as the variable
+    const mtgreminder = document.createElement('div');
+    header.appendChild(mtgreminder);
+    // create span that will hold the reminder
+    const remindertext = document.createElement('span');
+    // Set text of the span
+    remindertext.textContent = '🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.';
+    // make the remindertext span a child of the div for the reminder
+    mtgreminder.appendChild(remindertext);
+
+    // Set styling for the reminder
+    mtgreminder.style.color = "white";
+    mtgreminder.style.backgroundColor = "#0490fb";
+    mtgreminder.style.fontSize = ".7rem";
+    mtgreminder.style.justifyContent = "center";
+    mtgreminder.style.margin = "auto";
+    mtgreminder.style.gridRow = "1";
+	mtgreminder.style.gridColumn = "1/5";
+}
