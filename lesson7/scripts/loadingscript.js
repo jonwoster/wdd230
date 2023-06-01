dc775f32400abcf6ac6551d30b555bce4038ccf2
Jonathan Woster <jonathanwoster@gmail.com>
2023-06-01 16:06:04 -0500

    setup lazy loading for week 7

---
+++ lesson7/scripts/loadingscript.js
@@ -0,0 +1,35 @@
+
+const imagesToLoad = document.querySelectorAll("img[data-src]");
+
+const loadImages = (image) => {
+  image.setAttribute("src", image.getAttribute("data-src"));
+  image.onload = () => {
+    image.removeAttribute("data-src");
+  };
+};
+
+const options = {
+    rootMargin: "0px",
+    threshold: 0.75,
+  };
+
+if ("IntersectionObserver" in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+      items.forEach((item) => {
+        if (item.isIntersecting) {
+          loadImages(item.target);
+          observer.unobserve(item.target);
+        }
+      });
+    }, options);
+
+    imagesToLoad.forEach((img) => {
+      observer.observe(img);
+    });
+  } 
+else {
+    imagesToLoad.forEach((img) => {
+      loadImages(img);
+    });
+}
+
