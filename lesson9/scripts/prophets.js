9f7f9959861090bfa9096bd16ad71baad23fd7a1
Jonathan Woster <jonathanwoster@gmail.com>
2023-06-15 15:36:46 -0500

    formatting for prophets api page

---
+++ lesson9/scripts/prophets.js
@@ -0,0 +1,45 @@
+const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
+
+async function getProphetData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.table(data.prophets);  // note that we reference the prophet array of the data object given the structure of the json file
+    displayProphets(data.prophets);
+}
+  
+const displayProphets = (prophets) => {
+    const cards = document.querySelector('div.cards'); // select the output container element
+  
+    prophets.forEach((prophet) => {
+      // Create elements to add to the div.cards element
+      let card = document.createElement('section');
+      let h2 = document.createElement('h2');
+      let dateofbirth = document.createElement('p');
+      let placeofbirth = document.createElement('p');
+      let portrait = document.createElement('img');
+  
+      // Build the h2 content out to show the prophet's full name - finish the template string
+      h2.textContent = `${prophet.name} ${prophet.lastname}`;
+      dateofbirth.textContent = `Date of Birth: ${prophet.birthdate}`;
+      placeofbirth.textContent = `Place of Birth:${prophet.birthplace}`;
+  
+      // Build the image portrait by setting all the relevant attribute
+      portrait.setAttribute('src', prophet.imageurl);
+      portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname} `);
+      portrait.setAttribute('loading', 'lazy');
+      portrait.setAttribute('width', '340');
+      portrait.setAttribute('height', '440');
+  
+      // Append the section(card) with the created elements
+      card.appendChild(h2);
+      card.appendChild(dateofbirth);
+      card.appendChild(placeofbirth);
+      card.appendChild(portrait);
+      cards.appendChild(card);
+
+    }); // end of forEach loop
+  } // end of function expression
+
+
+getProphetData();
+
