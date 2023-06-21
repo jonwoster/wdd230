781b36c576803b3e82215d3ad16e63f57ec78ba4
Jonathan Woster <jonathanwoster@gmail.com>
2023-06-21 16:29:58 -0500

    Created weather API page for lesson 10

---
+++ lesson10api/scripts/weatherapi.js
@@ -0,0 +1,36 @@
+// select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const weatherurl = "https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&appid=8f6afa3a62d9258fbdaeb86c6417ea2e";
+
+async function apiFetch() {
+    try {
+      const response = await fetch(weatherurl);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data); // this is for testing the call
+        displayResults(data);
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  }
+  
+function displayResults(weatherData) {
+    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const desc = weatherData.weather[0].description;
+  
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = desc;
+}
+
+
+apiFetch();
+
